ae35fa7f49dd49b95b446d23c64d5c6c50945020
Pratik Kshirsagar <32354353+Pratikkshirsagar@users.noreply.github.com>
2018-10-11 23:37:28 +0530

    Algoritm added

---
+++ printing_steps/index.js
@@ -0,0 +1,42 @@
+
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+
+
+
+ function steps(n) {
+  for(let row=0; row < n;row++ ){
+    let s = "";
+
+    for(let column = 0; column < n; column++){
+      if(column <= row){
+        s += '#'
+      }else{
+        s += " "
+      }
+    }
+
+    console.log(s)
+  }
+
+}
+
+
+console.log(steps(4))
