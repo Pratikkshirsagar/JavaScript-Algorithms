c3418744da1590b98d428a9114a6a66209f6da0a
Pratikkshirsagar <meetpratiik@gmail.com>
2019-05-03 13:18:22 +0530

    add function updated

--- addTwoNumber/script.js
+++ addTwoNumber/script.js
@@ -3,5 +3,14 @@ function addTwoNumbers (num1,num2) {
 }
 
 
+function add(...num) {
+    let result = 0;
+    num.forEach(el => {
+        result += el
+    })
+    return result
+} 
+
+console.log(add(1,2,3,4,5))
 console.log( addTwoNumbers(5,6))
 console.log( addTwoNumbers(3,11))
