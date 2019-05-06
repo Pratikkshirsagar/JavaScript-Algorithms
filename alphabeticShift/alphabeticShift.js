d690226baf3360b0d7ad5fc0649e958c49e47efd
Pratikkshirsagar <meetpratiik@gmail.com>
2019-05-06 23:50:37 +0530

    algo aded

--- alphabeticShift/alphabeticShift.js
+++ alphabeticShift/alphabeticShift.js
@@ -16,3 +16,26 @@ function alphabeticShift(inputString) {
 }
 
 console.log(alphabeticShift('crazy'));
+
+// solution two
+
+function alphabeticShift2(inputString) {
+  let alphabet = {
+    a:'b', b:'c', c:'d',d:'e',
+    e:'f', f:'g',g:'h',h:'i',
+    i:'j',j:'k',k:'l',l:'m',
+    m:'n',n:'o',o:'p',p:'q',
+    q:'r',r:'s', s:'t',t:'u',
+    u:'v',v:'w',w:'x',x:'y',
+    y:'z', z:'a'
+  }
+  let inputShifted = inputString.split('')
+  for(let i=0; i < inputShifted.length; i++) {
+    inputShifted[i] =alphabet[inputShifted[i]]
+    
+  }
+
+  return inputShifted.join('')
+}
+
+console.log(alphabeticShift2('crazy'));
